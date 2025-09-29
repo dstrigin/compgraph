d688ca12342eede4e2f75b14622e3d7728da062a
dstrigin <dmitriystrigin04@gmail.com>
2025-09-29 10:29:46 +0300

    lab3 template

---
+++ lab-03/task1.py
@@ -0,0 +1,7 @@
+class ColorFiller:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Преобразование в оттенки серого")
+        self.root.geometry("1000x750+200+100")
+        self.root.minsize(800, 600)
+        
---
+++ lab-03/task3.py
@@ -0,0 +1,7 @@
+class FadeFiller:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Градиентное окрашивание")
+        self.root.geometry("1000x750+200+100")
+        self.root.minsize(800, 600)
+        
